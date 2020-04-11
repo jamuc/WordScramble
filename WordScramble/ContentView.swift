4024150b56ed6e18b2d707f2ecf3b1a5d24e22ec
Jason Franklin <jason.franklin@xing.com>
2020-04-11 14:03:04 +0200

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jason Franklin on 11.04.20.
+//  Copyright © 2020 Jason Franklin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
